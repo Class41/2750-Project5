aac6d8ab2f939e262085ef62e4c200450e8c23bc
vasyl onufriyev <projectclass41@gmail.com>
2018-10-24 14:02:04 -0500

    Added reading/prompt capability in part 2

--- part2/phone.c
+++ part2/phone.c
@@ -1,1 +1,39 @@
+//# Vasyl Onufriyev
+//# CS2750 PA5
+//# 10-24-18
 
+#include <stdio.h>
+#include "phone_fmt.h"
+
+int main()
+{
+   char input[200];
+   printf("Input: ");
+
+   scanf("%s", input);
+   
+   int i = 1;
+
+   int phvalues[10];
+   int phcnt = 0;
+
+   while(input[i] != '\0')
+   {
+      if(isdigit(input[i]))
+      {
+         if(phcnt >= 9)
+            break;
+
+         phvalues[phcnt] = input[i];
+         phcnt++;
+      }
+      i++;
+   }
+
+   if(phcnt < 9)
+   {
+      puts("Output: ERROR: Not enough digits on input!");
+   }
+
+   return 0;
+}
